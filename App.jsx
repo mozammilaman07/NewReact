a0aee680ac2c4dc4a99f45875be023357023637d
Md Mozammil Aman <amanmozammil.07@gmail.com>
2024-07-25 10:39:45 +0530

    a new component changed

--- App.jsx
+++ App.jsx
@@ -1,10 +1,12 @@
 import NewButton from "./button";
+import Hello from "./Hello";
 
 function App(){
   return <div>
     <h1>Welcome to new React </h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime quidem sint architecto autem qui sed blanditiis quibusdam recusandae. Perspiciatis eum voluptatem ducimus? Fugiat, quasi a esse iusto aperiam voluptates praesentium.</p>
     <NewButton></NewButton>
+    <Hello></Hello>
   </div>
 }
 
