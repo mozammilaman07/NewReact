1c0cf96c349688b2f4526d5cf4c5c25bd43d4c35
Md Mozammil Aman <amanmozammil.07@gmail.com>
2024-07-25 10:38:44 +0530

    added a component

---
+++ Hello.jsx
@@ -0,0 +1,8 @@
+function Hello(){
+    return <div>
+        <h3>Hello, this is a react app</h3>
+        <input type="checkbox" name="hello" id="" /><span><p>Check this box</p></span>
+    </div>
+}
+
+export default Hello;
