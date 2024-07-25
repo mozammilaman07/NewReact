d78bf1ab655960c5b7d990cc159d52b96098b91b
Md Mozammil Aman <amanmozammil.07@gmail.com>
2024-07-25 10:47:37 +0530

    added a input tag

--- Hello.jsx
+++ Hello.jsx
@@ -1,7 +1,8 @@
 function Hello(){
     return <div>
         <h3>Hello, this is a react app</h3>
-        <input type="checkbox" name="hello" id="" /><span><p>Check this box</p></span>
+        <span><input type="checkbox" name="hello" id="" /></span><p>Check this box</p>
+        <input type="text" name="" id="" placeholder="Enter a text here" />
     </div>
 }
 
