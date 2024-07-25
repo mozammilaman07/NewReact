3aa16457b18a32513f42d90213f5e1bf0ce824bf
Md Mozammil Aman <amanmozammil.07@gmail.com>
2024-07-25 10:27:07 +0530

    added new App.jsx file

---
+++ App.jsx
@@ -0,0 +1,11 @@
+import NewButton from "./button";
+
+function App(){
+  return <div>
+    <h1>Welcome to new React </h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime quidem sint architecto autem qui sed blanditiis quibusdam recusandae. Perspiciatis eum voluptatem ducimus? Fugiat, quasi a esse iusto aperiam voluptates praesentium.</p>
+    <NewButton></NewButton>
+  </div>
+}
+
+export default App;
